8623bb95c15e4eeb08df22437b115b032ba758ad
michellylopes00 <michelly.9607774@aluno.mg.gov.br>
2025-10-30 17:36:56 -0300

    gerenciar usuário

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+async function cadastarCardapio(cadastrar) {
+    
+}
+
+async function alterarCardapio(id) {
+    
+}
+
+async function excluirCardapio(id) {
+    
+}
+async function buscarCardapio() {
+    
+}
+
+function exibirTabelaCardapio(cardapio){
+
+}
+
+async function listarCardapio() {
+    
+}
